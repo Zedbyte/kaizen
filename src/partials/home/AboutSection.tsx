0031848a8c0ca42579b1093c2b630f19dde0dc0d
Aetrix <markjeromesantos102@gmail.com>
2025-01-13 16:50:22 +0800

    path changes

--- src/partials/home/AboutSection.tsx
+++ src/partials/home/AboutSection.tsx
@@ -2,9 +2,9 @@ import Timeline from "@/components/custom/Timeline"
 import { experienceData } from "@/constants/data"
 import { Link } from "react-router-dom"
 import SectionTitle from "../../components/custom/SectionTitle"
-import bentoAsset1 from "/public/assets/bento/b4.svg"
-import bentoAsset2 from "/public/assets/bento/b5.svg"
-import bentoGrid from "/public/assets/bento/grid.svg"
+import bentoAsset1 from "/assets/bento/b4.svg"
+import bentoAsset2 from "/assets/bento/b5.svg"
+import bentoGrid from "/assets/bento/grid.svg"
 import GitHubActivityCalendar from "../../components/custom/GithubCalendar";
 
 export default function AboutSection() {
--- src/partials/home/TestimonialsSection.tsx
+++ src/partials/home/TestimonialsSection.tsx
@@ -29,34 +29,34 @@ const testimonialsProfessionals = [
         "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
         name: "Mrs. Lilibeth Cuison",
         title: "Dean, AUF College of Computer Studies",
-        img: "/public/assets/icons/dark/aws.svg"
+        img: "/assets/icons/dark/aws.svg"
     },
     {
         quote:
         "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
         name: "Mrs. Melissa Pantig",
         title: "Program Chair, AUF College of Computer Studies",
-        img: "/public/assets/icons/dark/aws.svg"
+        img: "/assets/icons/dark/aws.svg"
     },
     {
         quote: "All that we see or seem is but a dream within a dream.",
         name: "Mr. James Esquivel",
         title: "Computer Science PhD, AUF College of Computer Studies",
-        img: "/public/assets/icons/dark/aws.svg"
+        img: "/assets/icons/dark/aws.svg"
     },
     {
         quote:
         "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
         name: "Mr. Joey Aviles",
         title: "Graduate School, AUF College of Computer Studies",
-        img: "/public/assets/icons/dark/aws.svg"
+        img: "/assets/icons/dark/aws.svg"
     },
     {
         quote:
         "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
         name: "Mr. Romack Natividad",
         title: "Senior Software Engineering Specialist",
-        img: "/public/assets/icons/dark/aws.svg"
+        img: "/assets/icons/dark/aws.svg"
     },
 ];
 
