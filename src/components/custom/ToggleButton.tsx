67f275e660eb53685c98b2712d216b956acd4c5f
Aetrix <markjeromesantos102@gmail.com>
2025-02-03 15:45:57 +0800

    Enhance dark mode support in ToggleButton component

--- src/components/custom/ToggleButton.tsx
+++ src/components/custom/ToggleButton.tsx
@@ -6,8 +6,8 @@ export default function ToggleButton({ isOn, setIsOn }: {
     return (
         <div className="absolute top-0 right-5">
             <div className="flex">
-                <button className={`${!isOn  ? 'bg-neutral-900' : 'bg-neutral-950' } p-1 cursor-pointer border rounded-l-md
-            border-zinc-700 border-r-zinc-900 hover:bg-neutral-900 transition-colors duration-200`} onClick={() => setIsOn(false)}>
+                <button className={`${!isOn  ? 'bg-neutral-300 dark:bg-neutral-900' : 'bg-neutral-200 dark:bg-neutral-950' } p-1 cursor-pointer border rounded-l-md
+            border-zinc-400 dark:border-zinc-700 border-r-zinc-500 dark:border-r-zinc-900 bg-neutral-200 dark:hover:bg-neutral-900 transition-colors duration-200`} onClick={() => setIsOn(false)}>
                     <svg
                     width="1.5em"
                     height="1.5em"
@@ -17,8 +17,8 @@ export default function ToggleButton({ isOn, setIsOn }: {
                     <path d="M3 5v14h18V5H3m16 2v2H5V7h14m0 4v2H5v-2h14M5 17v-2h14v2H5z" />
                     </svg>
                 </button>
-                <button className={`${isOn  ? 'bg-neutral-900' : 'bg-neutral-950' } p-1 cursor-pointer border rounded-r-md
-            border-zinc-700 border-l-zinc-900 hover:bg-neutral-900 transition-colors duration-200`} onClick={() => setIsOn(true)}>
+                <button className={`${isOn  ? 'bg-neutral-300 dark:bg-neutral-900' : 'bg-neutral-200 dark:bg-neutral-950' } p-1 cursor-pointer border rounded-r-md
+            border-zinc-400 dark:border-zinc-700 border-l-zinc-500 dark:border-l-zinc-900 bg-neutral-200 dark:hover:bg-neutral-900 transition-colors duration-200`} onClick={() => setIsOn(true)}>
                     <svg
                         width="1.5em"
                         height="1.5em"
